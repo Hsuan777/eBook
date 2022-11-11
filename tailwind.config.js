0b2e11df579137d665a79b0c6cb27e0be4a5e6e6
Hazel <owo0726@gmail.com>
2022-11-11 16:33:16 +0800

    feat:第一次專案上傳

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#F0EEE9',
+        creamWhite: '#FAF8F3',
+        creamWhite2: '#FFFEFC',
+        primary: '#8CA187',
+        primary1: '#a2b39e',
+        secondary: '#687e63',
+        secondary1: '#CDD027',
+        secondary2: '#A5A824',
+        info: '#387B27',
+        link: '#589EF0',
+        brown: '#474B42',
+      }
+    },
+    container: {
+      center: true,
+      padding: "12px",
+    },
+  },
+  plugins: [
+    require("daisyUi")
+  ],
+  // daisyUI config (optional)
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "cream": '#F0EEE9',
+          "creamWhite": '#FAF8F3',
+          "creamWhite2": '#FFFEFC',
+          "primary": '#8CA187',
+          "primary1": '#a2b39e',
+          "secondary": '#687e63',
+          "secondary1": '#CDD027',
+          "secondary2": '#A5A824',
+          "info": '#387B27',
+          "link": '#589EF0',
+          "brown": '#474B42',
+        },
+      },
+      // "dark",
+      // "cupcake",
+    ],
+  },
+}
